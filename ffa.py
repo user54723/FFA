caeb9f1dc3a26666d9eee03165f3207fbb62832f
user54723 <user54723@github.com>
2017-01-05 16:47:57 +0100

    Addition des samples et modification du parsing

--- ffa.py
+++ ffa.py
@@ -6,13 +6,7 @@ import xml.etree.ElementTree as et
 import scipy.io.wavfile.read
         
 class WordList :
-    def __init__(self, name, description, words) :
-        try :
-            index = int(name.split(" ")[-2])
-            speaker_quality = name.split(" ")[-1][1:-1]
-        except : 
-            index = 0
-            speaker_quality = "U"
+    def __init__(self, name, index, speaker_quality, description, words) :
         self.name = name
         self.index = index
         self.description = description
@@ -20,18 +14,19 @@ class WordList :
         self.words = words
 
 class Word : 
-    def __init__(self, name, parent_wordlist, index, wave_data, duration, fech) :
+    def __init__(self, name, index, wave_file, wave_start_position, wave_duration) :
         self.name = name
-        self.parent_wordlist_name = parent_wordlist_name
         self.index = index
-        self.wave_data_location = wave_data_location
-        self.duration = duration
+        self.wave_file = wave_data
+        self.wave_start_position = wave_start_position
+        self.wave_duration = wave_duration
 
 def parseOtoSuiteWordList(otoSuiteFolder="./otosuite") :
     #est ce que le dossier existe
     try : os.chdir(otoSuiteFolder)
     except : return -1
     rootDir = os.path.abspath(os.curdir())
+    wordlists = []
     for folder in os.listdir(rootDir) :
         #est ce que le dossier contient un fichier de définition otosuite
         curdir = (os.path.sep).join([rootDir, folder])
@@ -43,23 +38,51 @@ def parseOtoSuiteWordList(otoSuiteFolder="./otosuite") :
         r = tree.getroot()
         for wordlist in r.findall(".//Wordlist") :
             wordlistname = wordlist.find("Name").text
+            try :
+                wordlistindex = int(wordlistname.split(" ")[-2])
+                speaker_quality = wordlistname.split(" ")[-1][1:-1]
+                wordlistname = " ".join(wordlistname.split(" ")[:-2])
+            except : 
+                wordlistindex = 0
+                speaker_quality = "U"
             description = wordlist.find("Description").text
             item_number = len(wordlist.find(".//WordDefinition"))
+            #On construit la liste de mot contenu dans la wordlist
             words = []
-            wave_file_name = None
-            wave_file_loc = None
-            wave_data = None
-            wave_fech = None
+            #les données audio seront ensuite stockée sous forme de fichier wav 
+            #compréssé (un par type de liste)
+            wave_file_name = word.find("FileName").text
+            wave_file = (os.path.sep).join([curdir, wave_file_name])
+            word_index = 0
             for word in wordlist.findall(".//WordDefinition") :
-                if wave_file_name == None :
-                    wave_file_name = word.find("FileName").text
-                    wave_file_loc = (os.path.sep).join([curdir, wave_file_name])
-                    wave_fech, data = scipy.io.wavfile.read(wave_file_loc)
-                    wave_data = data[:,0]
-                else : 
-                    assert wave_file_name == word.find("FileName").text
+                assert wave_file_name == word.find("FileName").text
                 word_name = word.find("Word").text
-                word_timepos = word.find("StartPosition").text
-                word_duration = word.find("Duration").text
+                wave_start_position = int(word.find("StartPosition").text)
+                wave_duration = int(word.find("Duration").text)
+                words.append(Word(word_name, word_index, wave_file, wave_start_position, wave_duration))
+                word_index += 1
+            wordlists
                 
-                
+import sys
+from PyQt5.QtCore import QObject, pyqtSlot, QVariant
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtQml import QQmlApplicationEngine
+
+class FFA(QObject):
+    def __init__(self, qmlContext, parent=None):
+        super(FFA, self).__init__(parent)
+        self.window = parent
+        self.qmlContext = qmlContext
+        
+if __name__ == "__main__":
+    app_qt = QApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    # Création d'un objet QQmlContext pour communiquer avec le code QML
+    qmlContext = engine.rootContext()
+    engine.load('ui.qml')
+    window = engine.rootObjects()[0]
+    py_FFA = FFA(qmlContext, window)
+    qmlContext.setContextProperty("py_FFA", py_FFA)
+    window.show()
+    sys.exit(app_qt.exec())
+                        
